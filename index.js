540c4e1f97499e26082cd8c82a481fa9fd4a0369
DjebbZ <khalid.jebbari@gmail.com>
2014-02-25 23:04:27 +0100

    Conditional module

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var id = require("./js/identity");
 console.log("id", id(3));
+
+if (false) {
+    require("./js/conditional")();
+}
---
+++ js/conditional.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+    console.log("I'm the conditional module.");
+};
