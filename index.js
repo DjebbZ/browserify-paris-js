9df84d2303e8b4d3382579e4ed42b4857ea816f5
DjebbZ <khalid.jebbari@gmail.com>
2014-02-25 23:07:49 +0100

    Node.js core module

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log("id", id(3));
 if (false) {
     require("./js/conditional")();
 }
+
+var url = require('url');
+console.log("location", location.href);
+console.log("parsed location", url.parse(location.href));
+console.log("parsed web url", url.parse("https://example.com/page1?nodejs=true&browserify=true", true));
