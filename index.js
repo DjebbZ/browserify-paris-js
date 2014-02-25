d6af9e49cd94e9e557a784997d14ad999d096d75
DjebbZ <khalid.jebbari@gmail.com>
2014-02-25 23:28:10 +0100

    Use browserify-shim for global variable

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ var url = require('url');
 console.log("location", location.href);
 console.log("parsed location", url.parse(location.href));
 console.log("parsed web url", url.parse("https://example.com/page1?nodejs=true&browserify=true", true));
+
+var globalVar = require("./js/globalVar.js");
+console.log("globalVar", globalVar);
+console.log("window.globalVar", window.globalVar);
+console.log("global.globalVar", global.globalVar);
---
+++ js/globalVar.js
@@ -0,0 +1,2 @@
+// A "normal" javascript file that exposes a global variable
+window.globalVar = 1;
